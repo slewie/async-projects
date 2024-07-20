79c6e2cdc35b42e0e9ac75dc2a6ddb7c79c3b3c6
slewie <vladkowww@gmail.com>
2024-07-20 13:27:14 +0300

    Baseline code with proper async and error handling

--- web_scraper/test_file.py
+++ web_scraper/test_file.py
@@ -8,10 +8,21 @@ async def test_scraper():
     """
     Function doc
     """
-    urls = ["http://example1.com", "http://example2.com", "http://example3.com"]
+    urls = [
+        'https://www.crummy.com/software/BeautifulSoup/bs4/doc/'
+        'https://docs.aiohttp.org/en/stable/index.html',
+        'https://superfastpython.com/python-asyncio/#Coroutines_in_Python',
+        'https://www.python.org/dev/peps/pep-0492/',
+        'https://docs.python.org/3/library/asyncio-dev.html',
+        'https://www.python.org/dev/peps/pep-0530/',
+        'https://www.python.org/dev/peps/pep-0534/',
+        'https://www.python.org/dev/peps/pep-0537/',
+        'https://www.evidentlyai.com/ml-system-design',
+        'https://huyenchip.com/ml-interviews-book/contents/chapter-1.-ml-jobs.html'
+    ]
     scraper = AsyncWebScraper(urls)
     results = await scraper.run()
-
+    
     assert len(results) == len(urls)
     assert all(isinstance(result, dict) for result in results)
     assert all("title" in result for result in results)
--- web_scraper/web_scraper.py
+++ web_scraper/web_scraper.py
@@ -1,18 +1,74 @@
 """
-Module doc
+This module contains the classes and methods for scraping data from a website.
 """
-from typing import List
+from typing import List, Dict, Optional
+import asyncio
+import aiohttp
+from bs4 import BeautifulSoup
+import logging
 
 
 class AsyncWebScraper:
     """
-    Class doc
+    This class is used to scrape web pages asynchronously.
     """
 
     def __init__(self, urls: List[str]) -> None:
-        self.urls = urls
+        self.urls: List[str] = urls
+        
+        # TODO: add semaphores
+        # TODO: create a session once and reuse it for all requests.
+        
+        """
+        Possible solution:
+        
+        self.session = None  # Initialize session to None
 
-    def run(self):
+        async def __aenter__(self):
+            self.session = await aiohttp.ClientSession().__aenter__()
+            return self
+
+        async def __aexit__(self, exc_type, exc_val, exc_tb):
+            await self.session.__aexit__(exc_type, exc_val, exc_tb)
+            
+        async with AsyncWebScraper(urls) as scraper:
+            results = await scraper.run()
         """
-        Method doc
+
+    async def run(self) -> List[Dict[str, str]]:
         """
+        This method is used to run the scraper. It runs the scraper for the all websites in the list.
+        """
+        tasks = [self._get_and_scrap(url) for url in self.urls]
+        return await asyncio.gather(*tasks)
+        
+    async def _get_and_scrap(self, url: str) -> List[Dict[str, str]]:
+        """
+        This method is used to scrape the web page. It takes a url as input and returns a list of dictionaries.
+        """
+        html = await self._get_html(url)
+        return await self._scrap(html)
+        
+    async def _scrap(
+        self, html: str, scrapped_tags: Optional[List[str]] = None
+    ) -> Dict[str, str]:
+        """
+        This method is used to scrape the html using beautiful soup.
+        """
+        if scrapped_tags is None:
+            scrapped_tags = ["title", "h1", "h2", "h3", "p"]
+        soup = BeautifulSoup(html, features="html.parser")
+
+        return {tag.name: tag.text for tag in soup.find_all(scrapped_tags)}
+        
+    async def _get_html(self, url: str) -> str:
+        """
+        This method is used to get the html from a website.
+        """
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    return await response.text()
+        except aiohttp.ClientError as e:
+            logging.error(f"Request to the {url} failed: {e}")
+            return ""
